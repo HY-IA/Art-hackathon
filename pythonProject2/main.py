06cce29cf4b5a4c9bc1216d3edf82fd87e436ead
Mohamed Raid Athamena <jg20659@bristol.ac.uk>
2023-02-18 19:54:07 +0400

    machine

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -18,18 +18,36 @@ high = sleep_data[sleep_data.sl == 4]
 X = sleep_data.drop(columns="sl", axis = 0)
 Y = sleep_data["sl"]
 
-X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2, stratify = Y, random_state=5)
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.5, stratify = Y, random_state=5)
 
-model = LogisticRegression()
-model.max_iter=1000
+model = LogisticRegression(max_iter=100000)
 
-model.fit(X_train,Y_train)
-f = X_test.
-X_prediction = model.predict(X_test)
-training_accuracy = accuracy_score(X_prediction,Y_test)
-print(X_test)
+
+model.fit(X,Y)
+
+x= input("whats your sr?")
+x2= input("whats your rr?")
+x3= input("whats your t?")
+x4= input("whats your lm?")
+x5= input("whats your bo?")
+x6= input("whats your rem?")
+x7= input("whats your sr.1?")
+x8= input("whats your hr?")
+file = open("test.csv", "a")
+file.write(x+","+x2+","+x3+","+x4+","+x5+","+x6+","+x7+","+x8)
+
+file.close()
+input1 = pd.read_csv("test.csv")
+print(input1)
+X_prediction = model.predict(input1)
+
+
 print(X_prediction)
-print(Y_test)
+
+
+
+
+
 
 
 
